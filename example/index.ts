62bf7acbdb79beb70e3c152a4391494437c5a7a6
Jun Kato <i@junkato.jp>
2019-07-28 04:25:02 +0900

    Show file selection dialog

--- example/index.ts
+++ example/index.ts
@@ -1,2 +1,11 @@
 import { hello } from "../";
 hello();
+
+const input = document.querySelector("input") as HTMLInputElement;
+input.addEventListener("change", handleFileSelect);
+
+function handleFileSelect() {
+  if (input.files.length <= 0) return;
+  const file = input.files.item(0);
+  // Array.from(input.files).forEach(console.log.bind(console));
+}
